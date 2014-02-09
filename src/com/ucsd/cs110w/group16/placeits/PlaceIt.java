d11b37db955b87f987023a600242577c036031ee
Trevor Smith <tas002@ucsd.edu>
2014-02-09 00:38:48 +0000

    PlaceIt.java created online with Bitbucket

---
+++ src/com/ucsd/cs110w/group16/placeits/PlaceIt.java
@@ -0,0 +1,15 @@
+
+
+public class PlaceIt
+{
+	public String getName() {}
+	public String getDescription() {}
+	
+	public bool getStatus() {}
+	
+	public String getAlarmID() {}
+	public String getGeofenceID() {}
+	
+	public double getLatitude() {}
+	public double getLongitude() {}
+}
