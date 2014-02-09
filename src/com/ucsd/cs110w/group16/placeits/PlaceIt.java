baffcc657e063129529106980c27eae5617b82dd
Austin <Austin@169.228.179.250>
2014-02-08 17:13:08 -0800

    Updated PlaceIt with comments

--- src/com/ucsd/cs110w/group16/placeits/PlaceIt.java
+++ src/com/ucsd/cs110w/group16/placeits/PlaceIt.java
@@ -8,69 +8,92 @@ public class PlaceIt
 	int ID;
 	double longitude, latitude;
 	
+	//Sets the name of the PlaceIt to be displayed on map
 	public void setName(String name)
 	{
 		this.name = name;
 	}
+
+	//Gets the name of the PlaceIt
 	public String getName()
 	{
 		return name;
 	}
 	
+	//Sets the description of the PlaceIt
 	public void setDescription(String desc)
 	{
 		this.description = desc;
 	}
+	
+	//Gets the description of the PlaceIt
 	public String getDescription()
 	{
 		return description;
 	}
 	
+	//Set the status of the PlaceIt
 	public void setStatus(boolean status)
 	{
 		this.status = status;
 	}
+	
+	//Get whether the PlaceIt is active or not
 	public boolean isActive() 
 	{
 		return status;
 	}
 	
+	//Set the Alarm ID for the PlaceIt
 	public void setAlarmID(String id)
 	{
 		this.alarmID = id;
 	}
+	
+	//Gets the Alarm ID for the PlaceIt
 	public String getAlarmID()
 	{
 		return alarmID;
 	}
 	
+	//Sets the GeoFence ID for the PlaceIt
 	public void setGeofenceID(String id)
 	{
 		this.geofenceID = id;
 	}
+	
+	//Gets the Geofence ID for the PlaceIt
 	public String getGeofenceID() 
 	{
 		return geofenceID;
 	}
 	
+	//Set the ID of the PlaceIt
 	public void setID(int id)
 	{
 		this.ID = id;
 	}
+	
+	//Get the ID of the PlaceIt
 	public int getID()
 	{
 		return ID;
 	}
 	
+	//Sets the position of the PlaceIt
 	public void setPosition(double longitude, double latitude)
 	{
 		this.longitude = longitude;
 		this.latitude = latitude;
 	}
+	
+	//Gets the Latitude of the PlaceIt
 	public double getLatitude()
 	{
 		return longitude;
 	}
+	
+	//Gets the Longitude of the PlaceIt
 	public double getLongitude() 
 	{
 		return latitude;
