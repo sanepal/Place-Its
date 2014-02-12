88c8691070112b33839f8007a8861b33b6d45a12
gta0004 <gta0004@gmail.com>
2014-02-12 14:11:37 -0800

    Add test project (Check facebook for how to set up)

---
+++ PlaceItsTest/src/com/ucsd/cs110w/group16/placeits/test/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.ucsd.cs110w.group16.placeits.test;
+
+import android.test.AndroidTestCase;
+import junit.framework.TestCase;
+
+public class MainActivityTest extends AndroidTestCase {
+    
+    public void failTest() {
+        fail("always fail");
+    }
+
+}
